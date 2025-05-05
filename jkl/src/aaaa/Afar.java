07e2ab1ba5301e9e6e68c6445a8c54303dec834d
91772 <91772@PRIYANKA>
2025-05-05 17:35:16 +0530

    New Class added for LOS Project

---
+++ jkl/src/aaaa/Afar.java
@@ -0,0 +1,12 @@
+package aaaa;
+
+public class Afar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Plan for What ");
+	}
+
+}
---
+++ jkl/src/main/Ab.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Ab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("Say hello to Java");
+	}
+
+}
